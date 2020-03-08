4d51b8ee59c8cea134f43361c79443ba18a31754
Yvonne Nkr <yvonnkr86@gmail.com>
2020-03-08 17:02:56 +0000

    setup new user regitration/signup

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,5 +1,9 @@
 const express = require('express');
 const { check, validationResult } = require('express-validator');
+const gravatar = require('gravatar');
+const bycrypt = require('bcryptjs');
+
+const User = require('../../models/User');
 
 const router = express.Router();
 
@@ -16,14 +20,43 @@ router.post(
       'Please enter a valid password min 6 characters'
     ).isLength({ min: 6 })
   ],
-  (req, res) => {
+  async (req, res) => {
     //validate inputs
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
       return res.status(422).json({ errors: errors.array() });
     }
 
-    res.status(201).json({ user: req.body });
+    const { name, email, password } = req.body;
+
+    try {
+      //check if user exists
+      let user = await User.findOne({ email });
+
+      if (user) {
+        return res
+          .status(400)
+          .json({ errors: [{ msg: 'user with this email already exists' }] });
+      }
+
+      //get user gravatar
+      const avatar = gravatar.url(email, { s: '200', r: 'pg', d: 'mm' });
+
+      //encrypt password
+      const hashedPassword = await bycrypt.hash(password, 12);
+
+      //create/save user
+      user = new User({ name, email, password: hashedPassword, avatar });
+
+      await user.save();
+
+      res.status(201).json({ message: 'User Created', user });
+
+      //return jwt
+    } catch (err) {
+      console.log(err.message);
+      res.status(500).json({ message: 'Server error' });
+    }
   }
 );
 
