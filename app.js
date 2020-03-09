7bb5d65ef526ce174883c69b259d620dcd188052
Yvonne Nkr <yvonnkr86@gmail.com>
2020-03-09 18:32:39 +0000

    set headers to allow CORS

--- app.js
+++ app.js
@@ -9,6 +9,17 @@ connectDB();
 //init middleware  --alternative to using bodyParser --no need to install bodyParser express covers it
 app.use(express.json({ extended: false }));
 
+//Solve CORS error  --Cross-Origin-Resource-Sharing --blocks sharing data/resources from different domains
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type, Accept, Authorization '
+  );
+  res.setHeader('Access-Control-Allow-Methods', 'GET,POST,PUT,PATCH,DELETE');
+  next();
+});
+
 //routes
 app.use('/api/users', require('./routes/api/users'));
 app.use('/api/auth', require('./routes/api/auth'));
